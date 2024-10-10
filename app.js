c388b4898a0eb8430691a30ffe9787a708b48f72
Dmitriy <148681959+phoenix-intensive@users.noreply.github.com>
2024-10-10 16:38:51 +0700

    Update app.

--- app.js
+++ app.js
@@ -101,10 +101,10 @@ passport.use(new JwtStrategy({
 app.use(passport.initialize()); // Инициализация Passport.js
 
 // Подключение маршрутов
-app.use("/api/articles", articleRoutes); 
+app.use("/api/articles", articleRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
-app.use("/api/comments", commentRoutes); 
+app.use("/api/comments", commentRoutes);
 app.use("/api/requests", requestRoutes);
 app.use("/api/user", userRoutes);
 
